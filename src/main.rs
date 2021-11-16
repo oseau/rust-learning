e16bafdb1ec1ba2901833af01b09fe83cdea46ce
yang <oseau@me.com>
2021-11-16 19:55:18 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = "Hello,".to_string();
+    let x = "world!";
+    let mut ints = vec![1, 2, 8, 9];
+    ints.push(11);
+    println!("{} {} {:?}", s, x, ints);
+    let p = Point { x: 10, y: 20 };
+    println!("point: {}x{}", p.x, p.y);
 }
